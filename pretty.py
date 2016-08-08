5ed2edd599b49ab09d8f11774e5fffd398490436
orhanoz <orhanoz91@gmail.com>
2016-08-08 23:06:45 +0100

    pretfiy in process

---
+++ pretty.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
+import requests
+
+def printtags(soup):
+    temp=[]
+    for tag in soup.find_all(True):
+        temp.append(str(tag.name))
+    print list(set(temp))
+    
+def printpage(soup):
+    print soup.prettify()
+    return printtags(soup)
+
+def userpick():
+    t=raw_input("Pick a tag to follow:") 
+    return SoupStrainer(t)
+
+def letsoup(selected_tag):
+    return BeautifulSoup(data,'html.parser',parse_only=selected_tag)
+
+
+url='http://ikcu.edu.tr/Duyuru'
+r=requests.get(url)
+data=r.text
+soup=BeautifulSoup(data,'html.parser')
+printpage(soup)
+selected_tag=userpick()
+newsoup=letsoup(selected_tag)
+print newsoup.prettify()
+
+
